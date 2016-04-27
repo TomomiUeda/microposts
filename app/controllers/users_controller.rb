13c31a94709c15bc58b0a2e1de7807a1e7e3a90e
tomomiueda <pretty_don0629@icloud.com>
2016-04-27 05:44:42 +0000

    kadai1

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,17 @@
 class UsersController < ApplicationController
+  include SessionsHelper
+  before_action :set_message, only: [:show,:edit, :update]
+
   
   def show # 追加
-     @user = User.find(params[:id])
   end
   
   def new
     @user = User.new
   end
+  
+  def edit
+  end  
   
   def create
     @user = User.new(user_params)
@@ -17,12 +22,27 @@ class UsersController < ApplicationController
       render 'new'
     end
   end
+  
+  def update
+    if @user.update(user_params)
+      # 保存に成功した場合はトップページへリダイレクト
+      flash[:success] ='Updated profile!'
+      redirect_to @user
+    else
+      # 保存に失敗した場合は編集画面へ戻す
+      render 'edit'
+    end
+  end
 
   private
 
   def user_params
     params.require(:user).permit(:name, :email, :password,
-                                 :password_confirmation)
+                                 :password_confirmation,:location)
   end
+  
+  def set_message
+    @user = User.find(params[:id])
+  end  
   
 end
